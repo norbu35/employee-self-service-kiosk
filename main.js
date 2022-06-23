5734b640a68eff0f895a78b1cf55cd637015e0d5
Daagii-en <82932555+Daagii-en@users.noreply.github.com>
2022-06-23 11:27:00 +0800

    login click add

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 var counter = 5;
-document.addEventListener("DOMContentLoaded", () => {
+
+function loginClick(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+
+if(username=="admin"&&password=="admin"){
+    alert("succes");
+    return false;
+}else{
     const loginAlert = document.querySelector("#alert");
     const TimeLimit = document.querySelector(".timeLimit");
-    document.querySelector("#login__button").addEventListener("click", e => {
-        e.preventDefault();
+    
         counter-=1;
         if(counter>0){
             document.getElementById("count").innerHTML = counter;
@@ -12,6 +19,23 @@ document.addEventListener("DOMContentLoaded", () => {
             loginAlert.classList.add("alert--hidden");
             TimeLimit.classList.remove("timeLimit-hidden");
         }
-    });
 
-});
+}
+}
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const loginAlert = document.querySelector("#alert");
+//     const TimeLimit = document.querySelector(".timeLimit");
+//     document.querySelector("#login__button").addEventListener("click", e => {
+//         e.preventDefault();
+//         counter-=1;
+//         if(counter>0){
+//             document.getElementById("count").innerHTML = counter;
+//             loginAlert.classList.remove("alert--hidden");
+//         }else if(counter==0){
+//             loginAlert.classList.add("alert--hidden");
+//             TimeLimit.classList.remove("timeLimit-hidden");
+//         }
+//     });
+
+// });
